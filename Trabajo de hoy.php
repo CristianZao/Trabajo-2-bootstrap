d80094cd09f15c951f35091cce43ceb549928dec
CristianZao <128343536+CristianZao@users.noreply.github.com>
2026-05-28 15:53:51 -0300

    Add files via upload

---
+++ Trabajo de hoy.php
@@ -0,0 +1,59 @@
+<HTML>
+
+<Head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</Head>
+
+<Body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 p-3 bg-warning">
+                <h3>nevbar</h3>
+                <p>Nunc vitae metus non velit aliquam rhoncus vel in leo.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-8 mx-auto p-3 bg-danger">
+                <h3>Hero</h3>
+                <p>Nunc vitae metus non velit aliquam rhoncus vel in leo.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 p-3 bg-success border border-dark">
+                item
+            </div>
+            <div class="col-lg-3 p-3 bg-success border border-dark">
+                item
+            </div>
+            <div class="col-lg-3 p-3 bg-success border border-dark">
+                item
+            </div>
+            <div class="col-lg-3 p-3 bg-success border border-dark">
+                item
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6 p-3 bg-secondary border border-light">
+                <h3>contact</h3>
+                <p>Nunc vitae metus non velit aliquam rhoncus vel in leo.</p>
+            </div>
+            <div class="col-lg-6 p-3 bg-secondary border border-light">
+                <h3>contact</h3>
+                <p>Nunc vitae metus non velit aliquam rhoncus vel in leo.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 p-3 bg-dark text-light">
+                <h3>footer</h3>
+                <p>Nunc vitae metus non velit aliquam rhoncus vel in leo.</p>
+            </div>
+        </div>
+
+    </div>
+</Body>
+
+</HTML>
